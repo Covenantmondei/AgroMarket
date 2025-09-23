bfb19df45d6ca1d0be7ec2d46f8c2dafa64a7f19
Covenant Monday <167206547+Covenantmondei@users.noreply.github.com>
2025-09-23 15:32:35 +0100

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,35 +1,11 @@
 from rest_framework import serializers
 from .models import Farmer, Profile
 
-# class UpdateProfileSerializer(serializers.Serializer):
-#     first_name = serializers.CharField(max_length=30, required=False)
-#     last_name = serializers.CharField(max_length=30, required=False)
-#     phone_number = serializers.CharField(max_length=15, required=False)
-#     address = serializers.CharField(max_length=255, required=False)
-#     role = serializers.ChoiceField(choices=[('FARMER', 'Farmer'), ('BUYER', 'Buyer')], required=True)
-
-#     def create(self, validated_data):
-#         user = self.context['request'].user
-#         Profile.objects.create( **validated_data)
-
-#         profile = Profile.objects.get(user_id=user.id)
-
-#         if validated_data.get("role") == 'FARMER' or 'Farmer':
-#             Farmer.objects.create(profile=profile, **validated_data)
-
 class UpdateProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'phone_number', 'address', 'role']
 
-    # def create(self, validated_data):
-    #     role = validated_data.get("role")
-    #     if role == 'FARMER':
-    #         user = self.context['request'].user
-    #         profile = Profile.objects.get(user_id=user.id)
-    #         Farmer.objects.create(profile=profile)
-
-    #     return super().create(validated_data)
 
     def create(self, validated_data):
         create = super().create(validated_data)
